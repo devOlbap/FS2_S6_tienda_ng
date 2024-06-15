e537b3b6d98c28a68ff9aafe5f85412c60df84a1
devOlbap <pa.garridoc@duocuc.cl>
2024-06-15 12:00:28 -0400

    cambiando el nav a modulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:  [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports:  [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
