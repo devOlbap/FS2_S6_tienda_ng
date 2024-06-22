c268621830f402e54feb8c940127ed33d8ce97c9
devOlbap <pa.garridoc@duocuc.cl>
2024-06-22 00:55:24 -0400

    implementando navbar

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
