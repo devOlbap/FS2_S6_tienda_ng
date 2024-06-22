7e979cee14c489444ba4fe6ff63d0597301d9504
devOlbap <pa.garridoc@duocuc.cl>
2024-06-22 00:55:12 -0400

    implementando la obtencion del usuario loggeado

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { UserServiceService } from '../../service/user/user-service.service';
+import { User } from '../../model/user';
+import { Rol } from '../../model/rol';
 
 @Component({
   selector: 'app-nav',
@@ -10,5 +13,45 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  public rol:Rol = {
+    id:0,
+    descripcion:'',
+  }
+  public user : User = {
+    id:0,
+    nombres:'',
+    apellidos:'',
+    username:'',
+    rol:this.rol,
+    pass:'',
+    calle:'',
+    numeracion:'',
+    correo:'',
+    fecha_nac:'',
+    comuna:''
+
+  };
+
+  constructor(private userService: UserServiceService){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.user = this.userService.getUserLog();
+    // console.log(this.user)
+
+    // this.user = this.user;
+
+    // this.userService.userLog$.subscribe(user => {
+    //   this.user = user;
+    // });
+  }
+  logout() {
+    // Lógica para cerrar sesión
+    this.userService.logout();
+    // Redirigir a la página de inicio de sesión
+  }
+  
 
 }
