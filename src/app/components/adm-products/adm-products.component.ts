0f5c9b71078cc950d5d3c61347d60e4291fcbdba
devOlbap <pa.garridoc@duocuc.cl>
2024-06-22 00:52:56 -0400

    creando formulario reactivo para crear nuevos productos adm

--- src/app/components/adm-products/adm-products.component.ts
+++ src/app/components/adm-products/adm-products.component.ts
@@ -5,6 +5,10 @@ import { Product } from '../../model/product';
 import { CommonModule } from '@angular/common';
 
 
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NavComponent } from '../nav/nav.component';
+
+
 function formatNumberToCurrency(number: number): string {
   let formattedNumber = number.toLocaleString('es-CL', { style: 'currency', currency: 'CLP' });
   formattedNumber = formattedNumber.replace('CLP', '').trim();
@@ -15,19 +19,44 @@ function formatNumberToCurrency(number: number): string {
 @Component({
   selector: 'app-adm-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, NavComponent],
   templateUrl: './adm-products.component.html',
   styleUrl: './adm-products.component.css'
 })
 export class AdmProductsComponent {
+
+
+  productoForm: FormGroup;
+
+
   //Función para formatear números
   formatNumberToCurrency = formatNumberToCurrency;
   productos: Product[] = [];
-
-  constructor(private productService: ProductServiceService) { }
 
   ngOnInit(): void {
     this.productos = this.productService.getProducts();
   }
 
+  constructor(private productService: ProductServiceService, private fb : FormBuilder) { 
+    this.productoForm = this.fb.group({
+      id:[''],
+      nombre:['',Validators.required],
+      descripcion:['',Validators.required],
+      valor:['',Validators.required],
+      img:['',Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if(this.productoForm.valid){
+
+      this.productoForm.get('id')?.setValue(this.productos.length+1, { emitEvent: false });
+      
+      this.productService.addProduct(this.productoForm.value);
+      // console.log(this.productoForm.value)
+    }
+  }
+  limpiarFormularioProducto(){
+    this.productoForm.reset();
+  }
 }
