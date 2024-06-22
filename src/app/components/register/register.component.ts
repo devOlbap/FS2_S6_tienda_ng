c22e662e043f42741645df41be21c8ac2364fd37
devOlbap <pa.garridoc@duocuc.cl>
2024-06-22 00:55:58 -0400

    solo implementando navbar

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
