d09e3240aefe925ae02963d391981103c35607ec
devOlbap <pa.garridoc@duocuc.cl>
2024-06-22 00:50:18 -0400

    quitando navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './components/nav/nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:  [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavComponent],
+  imports:  [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
