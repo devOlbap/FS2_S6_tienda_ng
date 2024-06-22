0f679b818404da574fd019ae84a0150301356226
devOlbap <pa.garridoc@duocuc.cl>
2024-06-22 00:53:14 -0400

    solo implementando navbar

--- src/app/components/adm-users/adm-users.component.ts
+++ src/app/components/adm-users/adm-users.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-adm-users',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './adm-users.component.html',
   styleUrl: './adm-users.component.css'
 })
