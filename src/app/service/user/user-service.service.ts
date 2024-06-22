a7f9ce7f6e69d23e3554ec5f97c0bb75c21c0bd9
devOlbap <pa.garridoc@duocuc.cl>
2024-06-22 00:56:27 -0400

    creando funciones para login y logout ...

--- src/app/service/user/user-service.service.ts
+++ src/app/service/user/user-service.service.ts
@@ -2,11 +2,33 @@ import { Injectable } from '@angular/core';
 
 import { User } from '../../model/user';
 import { Rol } from '../../model/rol';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+  
+  private rolLog:Rol = {
+    id:0,
+    descripcion:'',
+  }
+
+  public userLog : User = {
+    id:0,
+    nombres:'',
+    apellidos:'',
+    username:'',
+    rol:this.rolLog,
+    pass:'',
+    calle:'',
+    numeracion:'',
+    correo:'',
+    fecha_nac:'',
+    comuna:''
+
+  };
   private roles: Rol[] = [
     { id: 1, descripcion: 'Administrador' },
     { id: 2, descripcion: 'Usuario' },
@@ -54,8 +76,13 @@ export class UserServiceService {
       fecha_nac:'06-04-1995'
     },
   ];
+  private userLogSubject: BehaviorSubject<User | undefined>;
+  public userLog$: Observable<User | undefined>;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.userLogSubject = new BehaviorSubject<User | undefined>(this.userLog);
+    this.userLog$ = this.userLogSubject.asObservable();
+  }
 
   getUsers(): User[] {
     return this.users;
@@ -72,4 +99,39 @@ export class UserServiceService {
   getRoles(): Rol[] {
     return this.roles;
   }
+
+  getUserLog():User{
+    return this.userLog;
+  }
+
+  setUserBlank(){
+    this.userLog = {
+      id:0,
+      nombres:'',
+      apellidos:'',
+      username:'',
+      rol:this.rolLog,
+      pass:'',
+      calle:'',
+      numeracion:'',
+      correo:'',
+      fecha_nac:'',
+      comuna:''
+  
+    }
+  }
+  login(username: string, password: string): User | undefined {
+    const foundUser = this.users.find(user => user.username === username && user.pass === password);
+    if (foundUser) {
+      this.userLog = foundUser;
+      return foundUser;
+    }
+    return undefined; 
+  }
+
+  logout(){
+    this.setUserBlank();
+    this.router.navigate(['/login'])
+  }
+
 }
