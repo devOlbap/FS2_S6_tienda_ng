461a627bef951a3b3f6d25093873afd0f0ec7921
devOlbap <pa.garridoc@duocuc.cl>
2024-07-10 22:07:17 -0400

    agregando validaciones al registro de nuevos usuarios

--- src/app/components/adm-users/adm-users.component.ts
+++ src/app/components/adm-users/adm-users.component.ts
@@ -83,6 +83,29 @@ export class AdmUsersComponent {
   getUsers(){
     this.usuarios = this.userService.getUsuarios();
   }
+
+  isAtLeast15YearsOld(birthDateString: string): boolean {
+    const birthDate = new Date(birthDateString);
+    if (isNaN(birthDate.getTime())) {
+      throw new Error('Fecha de nacimiento no válida');
+    }
+  
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+  
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+  
+    return age >= 15;
+  }
+  hasAtLeastOneNumber(str: string): boolean {
+    const regex = /\d/;
+    return regex.test(str);
+  }
+
+
   /**
    * Funcion que obtiene todos los roles disponibles para su seleccion.
    */
@@ -97,17 +120,42 @@ export class AdmUsersComponent {
    * @returns null | undefined
    */
   submit(){
-    
-    // let n_rol = this.roles.find(rol_e => rol_e.id == this.userForm.get('rolcito')?.value );
+    let username = this.userForm.get('username')?.value;
+    let pass = this.userForm.get('pass')?.value;
 
-    if(true){
+    if(username.trim().length ==0){
+      alert('debe indicar un nombre de usuario.');
+      return
+    }
+
+    if(pass.trim().length == 0 ){
+      alert('Debe indicar la contraseña.');
+      return
+    }
+
+    if(pass.trim().length < 8){
+      alert('La contraseña deben tener un minimo de 8 caracteres.')
+      return
+    }
+    if(!this.hasAtLeastOneNumber(pass)){
+      alert('La contraseña deben tener al menos un número.')
+      return
+    }
+    if(!this.isAtLeast15YearsOld(this.userForm.get('fecha_nac')?.value)){
+      alert('Debe ser mayor a 15 años.!');
+      return
+    }
+    
+    let n_rol = this.roles.find(rol_e => rol_e.id == this.userForm.get('rolcito')?.value );
+
+    if(n_rol){
       const n_user :User = {
         id:this.userService.getUsuarios().length+1,
         nombres:this.userForm.get('nombres')?.value,
         apellidos:this.userForm.get('apellidos')?.value,
-        username:this.userForm.get('username')?.value,
-        rol: 0,
-        pass:this.userForm.get('pass')?.value,
+        username:username,
+        rol: n_rol.id,
+        pass:pass,
         calle:this.userForm.get('calle')?.value,
         numeracion:this.userForm.get('numeracion')?.value,
         correo:this.userForm.get('correo')?.value,
@@ -115,17 +163,30 @@ export class AdmUsersComponent {
         comuna:this.userForm.get('comuna')?.value
       }
 
-      
-
       if(this.modificar){
         this.userService.updateUser(n_user);
-        alert('Usuario: '+n_user.nombres+' '+n_user.apellidos+' con ROL: xx modificado exitosamente!.');
+        alert('Usuario: '+n_user.nombres+' '+n_user.apellidos+' modificado exitosamente!.');
         this.getUsers();
         this.limpiarForm();
         return
       }
+
+
+      let user = this.userService.getUserByUsername(username);
+
+      if(user){
+        alert('Ya existe un usuario con el nombre de usuario: '+user.username+'.');
+        return
+      }
+      
+      user = this.userService.getUserByEmail(this.userForm.get('correo')?.value.trim());
+      if(user){
+        alert('Ya existe un usuario con el correo : '+user.correo+'.');
+        return
+      }
+
       this.userService.addUser(n_user);
-      alert('Usuario: '+n_user.nombres+' '+n_user.apellidos+' con ROL: xx creado exitosamente!.');
+      alert('Usuario: '+n_user.nombres+' '+n_user.apellidos+' con ROL: '+n_rol.descripcion+'. Fue creado exitosamente!.');
       this.getUsers();
       this.limpiarForm();
       return
@@ -139,6 +200,10 @@ export class AdmUsersComponent {
     this.userForm.reset();
     this.userForm.get('username')?.enable();
     this.modificar = false;
+  }
+
+  getRolDescripcionById(id:number){
+    return this.userService.getRolById(id)?.descripcion;
   }
   
   /**
@@ -156,13 +221,16 @@ export class AdmUsersComponent {
     if(user){
       this.modificar = true;
 
+      let rol = this.userService.getRolById(user.rol);
+
+
       this.userForm.patchValue({
         nombres: user.nombres,
         apellidos: user.apellidos,
         username: user.username,
         correo: user.correo,
         pass: user.pass,
-        rolcito: 0,
+        rolcito: rol?rol.id:0,
         fecha_nac: user.fecha_nac,
         calle: user.calle,
         numeracion: user.numeracion,
@@ -174,5 +242,22 @@ export class AdmUsersComponent {
       // this.userForm
     }
 
+  }
+  delUser(id:number){
+
+    const usr = this.userService.getUserById(id);
+
+    if(!usr){
+      alert('No existe usuario con el identificador: '+id);
+      return
+    }
+
+    if(confirm('¿Estás seguro que deseas eliminar a: '+usr.nombres+' '+usr.apellidos+' ?')){
+      this.userService.deleteUser(id);
+      
+      this.getUsers();
+      alert('Usuario: '+usr.nombres+' '+usr.apellidos +'. Eliminado correctamente!.');
+    }
+    return
   }
 }
