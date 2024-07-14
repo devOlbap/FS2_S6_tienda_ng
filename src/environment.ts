321778280121daea415c5428682353cf3112dc85
devOlbap <pa.garridoc@duocuc.cl>
2024-07-13 21:53:39 -0400

    subiendo archivos environments

---
+++ src/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://firebasestorage.googleapis.com/v0/b/',
+    dominio_app:"tienda-1c239.appspot.com/",
+    token_productos:"4de3d34e-93f6-438c-ae63-382dbb058aec",
+    token_usuarios:"3bb07084-3177-4a05-ac11-a27c04892d47",
+    token_roles:"c0e1ebc0-558f-4d7a-af0b-a09b097dd39d",
+    token_carrito:"4137649e-4857-459a-ab18-64bfed791d62",
+    apiKey:"",
+    usrsh:"admin",
+    psssh:""
+};
